cb2e42e5852eb7e86c27a3c9b01ac0c9566250b9
Naganathan05 <umaiyalramesh@gmail.com>
2025-02-18 09:12:52 +0530

    Schema Updates Done.

--- Backend/database/migrations/001_schema.up.sql
+++ Backend/database/migrations/001_schema.up.sql
@@ -1,177 +1,139 @@
-CREATE TABLE IF NOT EXISTS userRole (
-    roleId SERIAL PRIMARY KEY,
-    roleName VARCHAR(255) NOT NULL
+-- Department Table
+CREATE TABLE deptData (
+    deptID SERIAL PRIMARY KEY,
+    deptName VARCHAR(255) NOT NULL UNIQUE
 );
 
-INSERT INTO userRole (roleName) VALUES ('ADMIN'), ('DEPT_HEAD'), ('OFFICE'), ('PROFESSOR'), ('Hari');
-
-CREATE TABLE IF NOT EXISTS departmentData (
-    deptId SERIAL PRIMARY KEY,
-    deptName VARCHAR(255) NOT NULL
-);
-
-INSERT INTO departmentData (deptName) VALUES ('CSE'), ('AIE'), ('CYS');
-
-CREATE TABLE IF NOT EXISTS managementData (
-    managerId SERIAL PRIMARY KEY,
-    managerEmail VARCHAR(255) NOT NULL UNIQUE,
-    managerPassword VARCHAR(255) NOT NULL,
-    deptId INT NOT NULL,
-    roleId INT NOT NULL,
-    managerFullName VARCHAR(255),
+-- Admin Table
+CREATE TABLE adminData (
+    adminID SERIAL PRIMARY KEY,
+    emailID VARCHAR(255) UNIQUE NOT NULL,
+    adminName VARCHAR(255) NOT NULL,
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    createdBy INT NOT NULL,
-    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    managerStatus CHAR(1) DEFAULT '1',
-    FOREIGN KEY (deptId) REFERENCES departmentData(deptId),
-    FOREIGN KEY (roleId) REFERENCES userRole(roleId),
-    FOREIGN KEY (createdBy) REFERENCES managementData(managerId)
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS studentData (
-    studentId SERIAL PRIMARY KEY,
+-- Faculty Table
+CREATE TABLE facultyData (
+    facultyID SERIAL PRIMARY KEY,
+    emailID VARCHAR(255) UNIQUE NOT NULL,
+    facultyName VARCHAR(255) NOT NULL,
+    deptID INT REFERENCES deptData(deptID) ON DELETE SET NULL,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Student Table
+CREATE TABLE studentData (
+    studentID SERIAL PRIMARY KEY,
+    rollNumber VARCHAR(50) UNIQUE NOT NULL,
+    emailID VARCHAR(255) UNIQUE NOT NULL,
     studentName VARCHAR(255) NOT NULL,
-    studentRollNumber VARCHAR(50) UNIQUE NOT NULL,
-    studentGender CHAR(1) NOT NULL DEFAULT 'N',
-    studentPhone CHAR(10) NOT NULL,
-    studentEmail VARCHAR(255) UNIQUE NOT NULL,
-    studentPassword VARCHAR(255) NOT NULL,
-    studentDob CHAR(10) NOT NULL,
-    studentDeptId INT NOT NULL,
-    studentSection CHAR(1) NOT NULL,
-    studentBatchStart CHAR(4) NOT NULL,
-    studentBatchEnd CHAR(4) NOT NULL,
-    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    createdBy INT NULL,
-    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    studentStatus CHAR(1) DEFAULT '1',
-    FOREIGN KEY (studentDeptId) REFERENCES departmentData(deptId),
-    FOREIGN KEY (createdBy) REFERENCES managementData(managerId)
+    startYear INT NOT NULL,
+    endYear INT NOT NULL,
+    deptID INT REFERENCES deptData(deptID) ON DELETE SET NULL,
+    section VARCHAR(10) NOT NULL,
+    semester INT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS forgotPasswordStudent (
-    studentId INT NOT NULL,
-    otp VARCHAR(6) NOT NULL,
-    expiresAt TIMESTAMP DEFAULT NOW() + INTERVAL '5 minutes',
-    FOREIGN KEY (studentId) REFERENCES studentData(studentId)
-);
-
-CREATE TABLE IF NOT EXISTS forgotPasswordManagement (
-    managerId INT NOT NULL,
-    otp VARCHAR(6) NOT NULL,
-    expiresAt TIMESTAMP DEFAULT NOW() + INTERVAL '5 minutes',
-    FOREIGN KEY (managerId) REFERENCES managementData(managerId)
-);
-
-CREATE TABLE IF NOT EXISTS courseData (
-    courseId SERIAL PRIMARY KEY,
-    courseCode VARCHAR(20) UNIQUE NOT NULL,
+-- Course Table
+CREATE TABLE courseData (
+    courseID SERIAL PRIMARY KEY,
+    courseCode VARCHAR(50) UNIQUE NOT NULL,
     courseName VARCHAR(255) NOT NULL,
-    courseDeptId INT NOT NULL,
+    courseDeptID INT REFERENCES deptData(deptID) ON DELETE CASCADE,
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    createdBy INT NOT NULL,
     updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    courseStatus CHAR(1) DEFAULT '1',
-    courseType CHAR(1) DEFAULT '1',
-    updatedBy INT NOT NULL,
-    FOREIGN KEY (courseDeptId) REFERENCES departmentData(deptId),
-    FOREIGN KEY (createdBy) REFERENCES managementData(managerId),
-    FOREIGN KEY (updatedBy) REFERENCES managementData(managerId)
+    courseType VARCHAR(1) NOT NULL,
+    updatedBy INT REFERENCES adminData(adminID) ON DELETE SET NULL
 );
 
-CREATE TABLE IF NOT EXISTS courseFaculty (
-    classroomId SERIAL PRIMARY KEY,
-    courseId INT NOT NULL,
-    managerId INT NOT NULL,
-    batchStart CHAR(4) NOT NULL,
-    batchEnd CHAR(4) NOT NULL,
-    section CHAR(1) NOT NULL,
-    isMentor CHAR(1) DEFAULT '0',
-    isActive CHAR(1) DEFAULT '1',
+-- Course-Faculty Mapping Table
+CREATE TABLE courseFaculty (
+    classroomID SERIAL PRIMARY KEY,
+    courseID INT REFERENCES courseData(courseID) ON DELETE CASCADE,
+    facultyID INT REFERENCES facultyData(facultyID) ON DELETE CASCADE,
+    section VARCHAR(10) NOT NULL,
+    semester INT NOT NULL,
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    createdBy INT NOT NULL,
+    createdBy INT REFERENCES adminData(adminID) ON DELETE SET NULL,
     updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updatedBy INT NOT NULL,
-    FOREIGN KEY (courseId) REFERENCES courseData(courseId),
-    FOREIGN KEY (managerId) REFERENCES managementData(managerId),
-    FOREIGN KEY (createdBy) REFERENCES managementData(managerId),
-    FOREIGN KEY (updatedBy) REFERENCES managementData(managerId)
+    updatedBy INT REFERENCES adminData(adminID) ON DELETE SET NULL
 );
 
-CREATE TABLE IF NOT EXISTS studentCourse (
-    studentId INT NOT NULL,
-    classroomId INT NOT NULL,
-    FOREIGN KEY (studentId) REFERENCES studentData(studentId),
-    FOREIGN KEY (classroomId) REFERENCES courseFaculty(classroomId)
+-- Meeting Table
+CREATE TABLE meetingData (
+    meetingID SERIAL PRIMARY KEY,
+    startTime TIMESTAMP NOT NULL,
+    endTime TIMESTAMP NOT NULL,
+    classroomID INT REFERENCES courseFaculty(classroomID) ON DELETE CASCADE,
+    meetingLink VARCHAR(255),
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    createdBy INT REFERENCES facultyData(facultyID) ON DELETE SET NULL,
+    meetingDescription TEXT
 );
 
-CREATE TABLE IF NOT EXISTS classRoomData (
-    classId SERIAL PRIMARY KEY,
-    classroomId INT NOT NULL,
-    classStartTime TIMESTAMP NOT NULL,
-    classEndTime TIMESTAMP NOT NULL,
-    classLink VARCHAR(255) NOT NULL,
-    classStatus CHAR(1) DEFAULT '1',
-    FOREIGN KEY (classroomId) REFERENCES courseFaculty(classroomId)
+-- Attendance Table
+-- ('0' - Absent, '1' - Present)
+CREATE TABLE attendanceData (
+    meetingID INT REFERENCES meetingData(meetingID) ON DELETE CASCADE,
+    studentID INT REFERENCES studentData(studentID) ON DELETE CASCADE,
+    isPresent VARCHAR(1) DEFAULT '0',
+    PRIMARY KEY (meetingID, studentID)
 );
 
-CREATE TABLE IF NOT EXISTS attendanceData (
-    classId INT NOT NULL,
-    studentId INT NOT NULL,
-    isPresent CHAR(1) DEFAULT '0',
-    FOREIGN KEY (classId) REFERENCES classRoomData(classId),
-    FOREIGN KEY (studentId) REFERENCES studentData(studentId)
-);
-
-CREATE TABLE IF NOT EXISTS quizData (
-    quizId SERIAL PRIMARY KEY,
-    classroomId INT NOT NULL,
+-- Quiz Table
+CREATE TABLE quizData (
+    quizID SERIAL PRIMARY KEY,
+    classroomID INT REFERENCES courseFaculty(classroomID) ON DELETE CASCADE,
     quizName VARCHAR(255) NOT NULL,
-    quizDescription TEXT NOT NULL,
-    quizData JSON NOT NULL,
-    isOpenForAll CHAR(1) DEFAULT '0',
+    quizDescription TEXT,
+    quizData TEXT,
+    isOpenForAll VARCHAR(1) DEFAULT '0',
     startTime TIMESTAMP NOT NULL,
     endTime TIMESTAMP NOT NULL,
-    duration VARCHAR(20),
+    quizDuration INT NOT NULL,
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    createdBy INT NOT NULL,
     updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updatedBy INT NOT NULL,
-    FOREIGN KEY (classroomId) REFERENCES courseFaculty(classroomId),
-    FOREIGN KEY (createdBy) REFERENCES managementData(managerId),
-    FOREIGN KEY (updatedBy) REFERENCES managementData(managerId)
+    createdBy INT REFERENCES facultyData(facultyID) ON DELETE SET NULL
 );
 
-CREATE TABLE IF NOT EXISTS quizSubmission (
-    quizSubmissionId SERIAL PRIMARY KEY,
-    quizId INT NOT NULL,
-    quizSubmissionData JSON NOT NULL,
-    studentId INT NOT NULL,
+-- Quiz Submission Table
+CREATE TABLE quizSubmissionData (
+    quizSubmissionID SERIAL PRIMARY KEY,
+    quizID INT REFERENCES quizData(quizID) ON DELETE CASCADE,
+    quizSubmissionData TEXT,
+    studentID INT REFERENCES studentData(studentID) ON DELETE CASCADE,
     marks INT,
-    isPublished CHAR(1) DEFAULT '0',
+    submissionTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    isPublished VARCHAR(1) DEFAULT '0',
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    createdBy INT NOT NULL,
-    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updatedBy INT NOT NULL,
-    FOREIGN KEY (quizId) REFERENCES quizData(quizId),
-    FOREIGN KEY (studentId) REFERENCES studentData(studentId),
-    FOREIGN KEY (createdBy) REFERENCES managementData(managerId),
-    FOREIGN KEY (updatedBy) REFERENCES managementData(managerId)
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS assignmentData (
-    assignmentId SERIAL PRIMARY KEY,
-    classroomId INT NOT NULL,
+-- Assignment Table
+CREATE TABLE assignmentData (
+    assignmentID SERIAL PRIMARY KEY,
+    classroomID INT REFERENCES courseFaculty(classroomID) ON DELETE CASCADE,
     assignmentName VARCHAR(255) NOT NULL,
-    assignmentDescription TEXT NOT NULL,
-    isOpenToAll CHAR(1) DEFAULT '0',
+    assignmentDescription TEXT,
+    assignmentData TEXT,
     startTime TIMESTAMP NOT NULL,
     endTime TIMESTAMP NOT NULL,
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    createdBy INT NOT NULL,
     updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updatedBy INT NOT NULL,
-    FOREIGN KEY (classroomId) REFERENCES courseFaculty(classroomId),
-    FOREIGN KEY (createdBy) REFERENCES managementData(managerId),
-    FOREIGN KEY (updatedBy) REFERENCES managementData(managerId)
+    updatedBy INT REFERENCES facultyData(facultyID) ON DELETE SET NULL
+);
+
+-- Assignment Submission Table
+CREATE TABLE assignmentSubmission (
+    assignmentSubmissionID SERIAL PRIMARY KEY,
+    assignmentID INT REFERENCES assignmentData(assignmentID) ON DELETE CASCADE,
+    studentID INT REFERENCES studentData(studentID) ON DELETE CASCADE,
+    marks INT,
+    isPublished BOOLEAN DEFAULT FALSE,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    createdBy INT REFERENCES facultyData(facultyID) ON DELETE SET NULL,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
